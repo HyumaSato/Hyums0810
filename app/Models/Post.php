819cecdd51d57aa09ebbc2579890bca4d01cf9ab
HyumaSato <yakekusosato0720@gmail.com>
2023-02-24 08:50:56 +0900

    記事編集

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,7 +13,7 @@ protected $fillable = [
     'body',
 ];
 
-public function getByLimit(int $limit_count = 2)
+public function getByLimit(int $limit_count = 5)
 {
     // updated_atで降順に並べたあと、limitで件数制限をかける
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
