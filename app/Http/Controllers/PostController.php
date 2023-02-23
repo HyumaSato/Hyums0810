b6bd54c08434e75e14ceb563ae245b925e184656
HyumaSato <yakekusosato0720@gmail.com>
2023-02-24 04:10:59 +0900

    ブログ投稿詳細画面実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,10 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getByLimit()]);  
-       
+        return view('posts/index')->with(['posts' => $post->getByLimit(2)]);  
     }
+    public function show(Post $post)
+{
+    return view('posts/show')->with(['post' => $post]);
+}
 }
